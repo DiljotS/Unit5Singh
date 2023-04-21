eb1fffe632564126712930146bb1891c89a0a201
Singh <730222@student.dusd.net>
2023-04-21 11:37:02 -0700

    continued

--- SinghUnit5/Assets/Scripts/GameManager.cs
+++ SinghUnit5/Assets/Scripts/GameManager.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private float spawnRate = 1.0f;
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    private int score;
+    public TextMeshProUGUI gameOverText;
+
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());  
+        score = 0;
+        StartCoroutine(SpawnTarget());
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
@@ -25,4 +36,5 @@ public class GameManager : MonoBehaviour
             Instantiate(targets[index]);
         }
     }
+
 }
