69a110b73eeb1dc01f61b88141f176c14fba9db9
Singh <730222@student.dusd.net>
2023-04-25 11:38:00 -0700

    continuing

--- SinghUnit5/Assets/Scripts/GameManager.cs
+++ SinghUnit5/Assets/Scripts/GameManager.cs
@@ -10,16 +10,21 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
     private int score;
     public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+
 
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        UpdateScore(0);
         StartCoroutine(SpawnTarget());
+        isGameActive = true;
     }
 
     public void GameOver()
     {
+        isGameActive = false;
         gameOverText.gameObject.SetActive(true);
     }
 
@@ -30,11 +35,16 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpawnTarget()
     {
-        while (true){
+        while (isGameActive){
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
     }
 
 }
