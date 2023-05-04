b211e08fb1e22a43309b81e12bc69b5af8dfc944
DiljotS <zekhigi@gmail.com>
2023-05-03 21:08:57 -0700

    Finished

--- SinghUnit5/Assets/Scripts/GameManager.cs
+++ SinghUnit5/Assets/Scripts/GameManager.cs
@@ -14,24 +14,31 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI gameOverText;
     public bool isGameActive;
     public Button restartButton;
+    public GameObject titleScreen; 
+    public TextMeshProUGUI livesText;
+    private int lives;
+    public GameObject pauseScreen;
+private bool paused;
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;
-        isGameActive = true;
-
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
+       
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        
+      //Check if the user has pressed the P key
+if (Input.GetKeyDown(KeyCode.P))
+{
+ChangePaused();
+}
+  
     }
     IEnumerator SpawnTarget()
     {
@@ -47,6 +54,17 @@ public class GameManager : MonoBehaviour
         scoreText.text = "Score: " + score;
     }
 
+    public void UpdateLives(int livesToChange)
+{
+lives += livesToChange;
+livesText.text = "Lives: " + lives;
+if (lives <= 0)
+{
+GameOver();
+}
+}
+
+
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
@@ -58,4 +76,34 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
+
+    public void StartGame(int difficulty) 
+    {
+         score = 0;
+        isGameActive = true;
+        spawnRate /= difficulty;
+
+        StartCoroutine(SpawnTarget());
+        UpdateScore(0);
+        UpdateLives(3);
+
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    void ChangePaused()
+{
+if (!paused)
+{
+paused = true;
+pauseScreen.SetActive(true);
+Time.timeScale = 0;
+}
+else
+{
+paused = false;
+pauseScreen.SetActive(false);
+Time.timeScale = 1;
+}
+}
+
 }
